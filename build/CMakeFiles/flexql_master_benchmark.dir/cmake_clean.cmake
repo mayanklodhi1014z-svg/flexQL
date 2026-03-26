d4be1a4f518634819a2e78738ccc91038aeb2d3e
Mayank Lodhi <230403174+mayanklodhi1014z-svg@users.noreply.github.com>
2026-03-26 11:49:48 +0530

    Add files via upload

---
+++ build/CMakeFiles/flexql_master_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/mayank/Videos/project_2_antigravity/bin/flexql_master_benchmark"
+  "/home/mayank/Videos/project_2_antigravity/bin/flexql_master_benchmark.pdb"
+  "CMakeFiles/flexql_master_benchmark.dir/scripts/master_benchmark.cpp.o"
+  "CMakeFiles/flexql_master_benchmark.dir/scripts/master_benchmark.cpp.o.d"
+  "CMakeFiles/flexql_master_benchmark.dir/src/client_api.cpp.o"
+  "CMakeFiles/flexql_master_benchmark.dir/src/client_api.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flexql_master_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
